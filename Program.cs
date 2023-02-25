2d7d5dfcb580db0b41cf404efc30593b87842de5
DVDeusVultDV <crazygamergmr@gmail.com>
2023-02-25 19:33:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+//app.MapGet("/", () => "Hello World!");
+app.Run(async (context) =>
+{
+	context.Response.ContentType = "text/html;";
+	switch (context.Request.Path)
+	{
+		case "/registration": await context.Response.SendFileAsync("html\\registration.html"); break;
+		case "/info":
+			var form = context.Request.Form;
+			List<string> languages = new List<string>(){ form["c#"], form["c++"] };
+			string str =
+			$"<h3>Info:</h3>" +
+			$"<p>Name: {form["name"]}</p>" +
+			$"<p>Second Name: {form["s_name"]}</p>" +
+			$"<h3>Languages:</h3>";
+			foreach (var i in form.Keys)
+				if (form[i] == "on") str += $"<p>{i}</p>";
+			await context.Response.WriteAsync(str);
+			break;
+		default: await context.Response.SendFileAsync("html/index.html"); break;
+	}
+	
+});
+app.Run();
